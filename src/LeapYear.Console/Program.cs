8101967bb8bb11fdaa300395d25546257499144d
Stefano Vidaurre Olite <svidaurre@domingoalonsogroup.com>
2023-03-10 12:28:48 +0100

    Feat: For the year 200, the IsLeapYear function return false

--- src/LeapYear.Console/Program.cs
+++ src/LeapYear.Console/Program.cs
@@ -17,7 +17,7 @@ public static class Program
             return true;
         }
 
-        if (year == 100)
+        if (year == 100 || year == 200)
         {
             return false;
         }
