1830537ec8c21f74c56ecd570b25da2d224ae602
Stefano Vidaurre Olite <svidaurre@domingoalonsogroup.com>
2023-03-10 11:41:21 +0100

    Test: Should return false for year 1

--- test/LeapYear.Test/LeapYearTest.cs
+++ test/LeapYear.Test/LeapYearTest.cs
@@ -8,9 +8,16 @@ public class LeapYearTest
     }
 
     [Test]
-    public void ShouldReturnTrueWithTheYear400()
+    public void ShouldReturnTrueForYear400()
     {
         bool result = Program.IsLeapYear(400);
         Assert.IsTrue(result);
+    }
+
+    [Test]
+    public void ShouldReturnFalseForYear1()
+    {
+        bool result = Program.IsLeapYear(1);
+        Assert.IsFalse(result);
     }
 }
